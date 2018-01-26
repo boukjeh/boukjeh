675efb909571aa9555cb1e43a0927792414f74c6
boukjeh <boukjeh123@gmail.com>
2018-01-26 15:57:27 +0100

    travis

--- app.py
+++ app.py
@@ -1,16 +1,8 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-import unittest
-from app import main
-
-
-class TestApp(unittest.TestCase):
-    def test_main(self):
-        self.assertEqual(main(), 'Hello World!')
-
+def main():
+    return 'Hello World!'
 
 if __name__ == '__main__':
-    unittest.main()
-
-#
+main()
